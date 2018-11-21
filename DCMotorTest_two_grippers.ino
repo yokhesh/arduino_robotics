5aa71d152581102355278c7f522cb37b89557887
Yokhesh KrishnasamyTamilselvam <ykrishn@g.clemson.edu>
2018-11-21 23:20:04 +0530

    Add files via upload

---
+++ DCMotorTest_two_grippers.ino
@@ -0,0 +1,60 @@
+// Adafruit Motor shield library
+// copyright Adafruit Industries LLC, 2009
+// this code is public domain, enjoy!
+
+// Two grippers
+
+#include <AFMotor.h>
+
+AF_DCMotor gripper1(1);
+AF_DCMotor gripper2(2);
+
+void setup() {
+  Serial.begin(9600);           // set up Serial library at 9600 bps
+
+  // turn on motor
+  gripper1.setSpeed(220);
+  gripper2.setSpeed(220);
+   
+  gripper1.run(RELEASE);
+  gripper2.run(RELEASE);
+}
+
+void loop() {
+  char c = ' ';
+
+  
+ // Gripper 1
+  if (Serial.available()) {
+    c  = Serial.read();
+    if (c == 'q') {
+      gripper1.run(FORWARD);
+      delay(1000);
+      gripper1.run(RELEASE);
+      Serial.println("Close gripper 1!");
+    }
+    if (c == 'w') {
+      gripper1.run(BACKWARD);
+      delay(1000);
+      gripper1.run(RELEASE);
+      Serial.println("Open gripper 1!");
+    }
+
+ // Gripper 2
+    
+    if (c == 'a') {
+      gripper2.run(FORWARD);
+      delay(1000);
+      gripper2.run(RELEASE);
+      Serial.println("Close gripper 2!");
+    }
+    if (c == 's') {
+      gripper2.run(BACKWARD);
+      delay(1000);
+      gripper2.run(RELEASE);
+      Serial.println("Open gripper 2!");
+    }
+    }
+ }
+
+ 
---
+++ proj2.ino
@@ -0,0 +1,21 @@
+
+int LMT_SW2 = 5; 
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(LMT_SW2, INPUT_PULLUP);
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+    Serial.print("ON");
+    delay(1000);
+    if ((digitalRead(LMT_SW2) == 0))
+    {
+      delay(1000);
+    Serial.print("Wheel Chair in position");
+    delay(1000);
+    Serial.print("Start the operation");
+    }
+
+}
